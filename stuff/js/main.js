aa35933d7101707490d6aec464e9a917153b3d21
Kenny Robstad <148172178+kenny-rob@users.noreply.github.com>
2023-12-05 18:41:23 -0700

    commit

--- stuff/js/main.js
+++
@@ -1,6 +0,0 @@
-/* comment
-multiline */
-
-//alert('Hello World!');
-
-const FirstName = "Kenny";
--- stuff/main.js
+++
@@ -1,9 +0,0 @@
-/* comment
-multiline */
-
-//alert('Hello World!');
-
-const FirstName = "Kenny"; 
-let lastName = "Robstad"; //ES6
-/ var lastName = "Robstad"; //ES5
-let age = ""
